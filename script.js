d8889e8950f2f71273209605ea6c4478698a5e36
veeresh8055 <visesync@gmail.com>
2026-03-03 20:08:50 +0530

    Windows-11 UI Upadated

---
+++ script.js
@@ -0,0 +1,81 @@
+function setTimeDate() {
+  let time = document.getElementById("time");
+  let dateDisplay = document.getElementById("date");
+
+  // console.log(new Date());
+
+  let date = new Date();
+
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  // console.log(hours)
+  // console.log(minutes)
+
+  time.innerHTML = `${hours}:${minutes}`;
+
+  let day = date.getDay() + 1;
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+
+//   console.log(day, month, year);
+  dateDisplay.innerHTML = `${day}-${month}-${year}`;
+}
+
+setInterval(setTimeDate, 1000);
+setTimeDate();
+function createSlider(containerId, barId, circleId) {
+  var container = document.getElementById(containerId);
+  var bar = document.getElementById(barId);
+  var circle = document.getElementById(circleId);
+
+  var isDragging = false;
+
+  circle.addEventListener("mousedown", function () {
+    isDragging = true;
+  });
+
+  document.addEventListener("mouseup", function () {
+    isDragging = false;
+  });
+
+  document.addEventListener("mousemove", function (e) {
+    if (isDragging) {
+      var rect = container.getBoundingClientRect();
+      var mouseX = e.clientX - rect.left;
+      var width = rect.width;
+
+      var percentage = (mouseX / width) * 100;
+
+      if (percentage < 0) percentage = 0;
+      if (percentage > 100) percentage = 100;
+
+      bar.style.width = percentage + "%";
+      circle.style.left = percentage + "%";
+    }
+  });
+}
+
+// Create two independent sliders
+createSlider("container1", "bar1", "circle1");
+createSlider("container2", "bar2", "circle2");
+
+
+const side = document.querySelector('.side');
+const toggle = document.querySelector('.taskbar .rightIcon .second');
+
+if (toggle && side) {
+  toggle.addEventListener('click', () => {
+    side.classList.toggle('hidden');
+  });
+}
+
+// menu image toggle (use `.open` class)
+const menu = document.querySelector('.menuBar');
+const menuImg = document.querySelector('.menuImg');
+
+if (menu && menuImg) {
+  menu.addEventListener('click', () => {
+    menuImg.classList.toggle('open');
+  });
+}
